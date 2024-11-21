cf69f070860a1911fac595b338b8bf1f33c88409
Victoria Ferreira <victoria.senac.13@gmail.com>
2024-11-21 16:02:37 -0300

    socorro

---
+++ src/olaMundo/maina.java
@@ -0,0 +1,22 @@
+package olaMundo;
+
+public class maina {
+    public static void main(String[] args) {
+        int y = 9;
+        int x = 10;
+
+        //Operadores Aritmeticos
+        System.out.println("y = " + y);
+        System.out.println("x = " + x);
+        System.out.println("y + x = " + (x+y));
+        System.out.println("x - y = " + (x-y));
+        System.out.println("x * y = " + (x*y));
+        System.out.println("x/y = " + ((float)x/y));
+        
+        //Operadores relacionais
+        // == igualdade
+        // != diferença
+        // <> menor e maior
+         
+    }
+}
---
+++ src/olaMundo/olaMundo.java
@@ -0,0 +1,28 @@
+package olaMundo;
+
+// declaração de classes
+public class olaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+    }
+
+
+// tipos de dados primitivos
+byte b = 0;
+short s = 20;
+int i = 200;
+long l = 2000;
+
+// Tipos da dados primitivos - numeros com ponto flutuante
+float f = 1.5f;
+double d = 2000.456;
+
+// Tipos de dados não numéricos
+char c = 'a';
+boolean t = true;
+boolean a = false;
+
+/*
+ * comentario em varias linhas
+ */
+}
