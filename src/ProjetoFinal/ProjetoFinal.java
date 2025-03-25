757a513f52deba67a036fcb26b43e15581fe9c2e
victoriafe-sa <victoria.senac.13@gmail.com>
2025-03-25 12:26:11 -0300

    projeto finalizado

--- src/ProjetoFinal/ProjetoFinal.java
+++ src/ProjetoFinal/ProjetoFinal.java
@@ -7,18 +7,28 @@ public class ProjetoFinal {
         v[1] = new Video("POO em PHP");
         v[2] = new Video("POO em Python");
         
-        System.out.println(v[0].toString());
-        System.out.println(v[1].toString());
-        System.out.println(v[2].toString());
-
         Gafanhoto g[] = new Gafanhoto[3];
-
         g[0] = new Gafanhoto("Maria", 18, "F", "maria");
         g[1] = new Gafanhoto("Jonas", 25, "M", "joh");
         g[2] = new Gafanhoto("Victoria", 18, "F", "vic");
 
+        Visualizacao vis[] = new Visualizacao[3];
+        vis[0] = new Visualizacao(g[0], v[0]);
+        vis[0].avaliar(32.5f);
+        vis[1] = new Visualizacao(g[0], v[1]);
+
+        System.out.println("VIZUALIZAÇÃO \n ------------------------");
+        System.out.println(vis[0].toString());
+        System.out.println(vis[1].toString());
+
+        /*
+        System.out.println("VIDEOS \n ------------------------");
+        System.out.println(v[0].toString());
+        System.out.println(v[1].toString());
+        System.out.println(v[2].toString());
+        System.out.println("GAFANHOTOS \n ------------------------");
         System.out.println(g[2].toString());
         System.out.println(g[0].toString());
-        System.out.println(g[1].toString());
+        System.out.println(g[1].toString());*/
     }
 }
